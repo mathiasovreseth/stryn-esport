39bccb8af648edcd1b158400563c3ad0fde0ee19
Michal Berg <michalb@stud.ntnu.no>
2022-11-24 19:18:41 +0100

    Implemented a custom widget for implementing bullet point lists.

--- lib/pages/becomeMemberPage/become_member_page.dart
+++ lib/pages/becomeMemberPage/become_member_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stryn_esport/widgets/appBars/custom_app_bar.dart';
+import 'package:stryn_esport/widgets/custom_bullet_list.dart';
 
 class BecomeMemberPage extends StatelessWidget {
   const BecomeMemberPage({super.key});
@@ -13,12 +13,64 @@ class BecomeMemberPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(
+    return Scaffold(
+      appBar: const CustomAppBar(
         headerText: "Bli medlem",
       ),
       body: Padding(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(20),
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 50,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10.0),
+                    topRight: Radius.circular(10.0),
+                  ),
+                  color: Colors.blue,
+                ),
+                child: const Text(
+                  "Hovedmedlem",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 1,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 15,
+                  top: 10,
+                ),
+                child: Text(
+                  "Priser",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+              ),
+              const BulletList(
+                strings: [
+                  "Test1",
+                  "Test2",
+                  "Test3",
+                  "Test4",
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
