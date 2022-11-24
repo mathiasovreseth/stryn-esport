dd268965bc5c6710d0948dd20f76a4c7e4eb4583
Michal Berg <michalb@stud.ntnu.no>
2022-11-24 19:33:06 +0100

    Refactored the becomeMemberPage.

--- lib/pages/becomeMemberPage/become_member_page.dart
+++ lib/pages/becomeMemberPage/become_member_page.dart
@@ -17,70 +17,78 @@ class BecomeMemberPage extends StatelessWidget {
       appBar: const CustomAppBar(
         headerText: "Bli medlem",
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Card(
-          color: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                height: 50,
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
-                  ),
-                  color: Colors.blue,
+      body: Column(
+        children: [
+          _buildBecomeMainMember(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBecomeMainMember() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 50,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
                 ),
-                child: const Text(
-                  "Hovedmedlem",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    letterSpacing: 1,
-                  ),
+                color: Colors.blue,
+              ),
+              child: const Text(
+                "Hovedmedlem",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 1,
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.only(
-                  left: 15,
-                  top: 10,
-                ),
-                child: Text(
-                  "Priser",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 0.5,
-                  ),
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 10,
               ),
-              const BulletList(
-                strings: [
-                  "Årskontigent: 995.-",
-                  "6 måneder: 595.-",
-                  "3 måneder: 295.-",
-                  "1 måned: 159.-",
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Vi har desverre ingen betalingsløsninger i appen, "
-                  "så vennligst kontakt xxx angående nytt medlemskap",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
+              child: Text(
+                "Priser",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5,
                 ),
               ),
-            ],
-          ),
+            ),
+            const BulletList(
+              strings: [
+                "Årskontigent: 995.-",
+                "6 måneder: 595.-",
+                "3 måneder: 295.-",
+                "1 måned: 159.-",
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "Vi har desverre ingen betalingsløsninger i appen, "
+                "så vennligst kontakt xxx angående nytt medlemskap",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
