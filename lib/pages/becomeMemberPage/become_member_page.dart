db6ae8dcb45c2860601262b4d2fb7c3c7e46aa94
Michal Berg <michalb@stud.ntnu.no>
2022-11-25 10:27:20 +0100

    Added become_support_member widget. Extracted becomeSupportMember and becomeMainMemberr widget to separate classes.

--- lib/pages/becomeMemberPage/become_member_page.dart
+++ lib/pages/becomeMemberPage/become_member_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stryn_esport/widgets/appBars/custom_app_bar.dart';
-import 'package:stryn_esport/widgets/custom_bullet_list.dart';
+import 'package:stryn_esport/widgets/become_main_member.dart';
+import 'package:stryn_esport/widgets/become_support_member.dart';
 
 class BecomeMemberPage extends StatelessWidget {
   const BecomeMemberPage({super.key});
@@ -18,79 +19,15 @@ class BecomeMemberPage extends StatelessWidget {
         headerText: "Bli medlem",
       ),
       body: Column(
-        children: [
-          _buildBecomeMainMember(),
+        children: const [
+          BecomeSupportMemberWidget(),
+          BecomeMainMemberWidget(),
+          SizedBox(height: 8.0),
         ],
       ),
     );
   }
-
-  Widget _buildBecomeMainMember() {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Card(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 50,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(10.0),
-                  topRight: Radius.circular(10.0),
-                ),
-                color: Colors.blue,
-              ),
-              child: const Text(
-                "Hovedmedlem",
-                style: TextStyle(
-                  fontSize: 20.0,
-                  letterSpacing: 1,
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 15,
-                top: 10,
-              ),
-              child: Text(
-                "Priser",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
-                ),
-              ),
-            ),
-            const BulletList(
-              strings: [
-                "Årskontigent: 995.-",
-                "6 måneder: 595.-",
-                "3 måneder: 295.-",
-                "1 måned: 159.-",
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "Vi har desverre ingen betalingsløsninger i appen, "
-                "så vennligst kontakt xxx angående nytt medlemskap",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
+
+
+
---
+++ lib/widgets/become_main_member.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:stryn_esport/widgets/custom_bullet_list.dart';
+
+class BecomeMainMemberWidget extends StatelessWidget {
+  const BecomeMainMemberWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 50,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                ),
+                color: Colors.blue,
+              ),
+              child: const Text(
+                "Hovedmedlem",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 1,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 10,
+              ),
+              child: Text(
+                "Priser",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.5,
+                ),
+              ),
+            ),
+            const BulletList(
+              strings: [
+                "Årskontigent: 995.-",
+                "6 måneder: 595.-",
+                "3 måneder: 295.-",
+                "1 måned: 159.-",
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "Vi har desverre ingen betalingsløsninger i appen, "
+                    "så vennligst kontakt xxx angående nytt medlemskap",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
